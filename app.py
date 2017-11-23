99875137d28c1889371cf3412d2f0bf2ddd5aa60
Brian Hammill <hammillbc@gmail.com>
2017-11-22 21:58:58 -0500

    cleaned out test images, spawning trees, enforcing grid

--- app.py
+++ app.py
@@ -41,23 +41,27 @@ def main():
     screen.blit(background, (0, 0))
     pygame.display.flip()
 
+    grid = [(x, y) for x in range(40, ZONERECT.right, 80) for y in range(40, ZONERECT.bottom, 80)]
+
     characters, char_default = load_json_def("characters.json")
     trees, tree_default = load_json_def("trees.json")
     create = extract_random_from_dict(characters, char_default)
-    test = drifting_object(pos=(100, 0), **create)
+    test = drifting_object(pos=grid[0], **create)
 
     balls = []
-    tree = base_object(pos=SCREENRECT.center, **trees["evergreen"])
-    balls.append(tree)
+    for x in range(10):
+        pos = grid.pop(random.randint(0, len(grid)-1))
+        tree = base_object(pos=pos, **trees["evergreen"])
+        balls.append(tree)
     for x in range(10):
         # balls.append(base_object(pos=(random.randint(0, ZONERECT.right), random.randint(0, ZONERECT.bottom)), **extract_from_dict("ball", characters, char_default)))
+        pos = grid.pop(random.randint(0, len(grid)-1))
         create = extract_random_from_dict(characters, char_default)
-        balls.append(base_object(pos=(random.randint(0, ZONERECT.right), random.randint(0, ZONERECT.bottom)), **create))
+        balls.append(base_object(pos=pos, **create))
 
-    # RenderPlain creates Sprite Group for chimp and mouse
     allsprites = pygame.sprite.RenderPlain((test,))
     allsprites.add(balls)
-    print(allsprites.sprites())
+
     # Clock controls frame rate
     clock = pygame.time.Clock()
 
@@ -106,6 +110,7 @@ def main():
             elif keystate[pygame.K_ESCAPE]:
                 key_press = "exit"
                 interacting = False
+                window.kill()
 
             choice = window.update(key_press)
             if choice:
